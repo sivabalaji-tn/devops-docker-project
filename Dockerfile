6232f8b911223443292464a87efe9bdeab8a682e
sivabalaji-tn <sivathetechie24@gmail.com>
2026-02-17 15:58:39 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM eclipse-temurin:17-jdk
+WORKDIR /app
+COPY . .
+RUN javac Main.java
+CMD ["java", "Main"]
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Im a Build pipeline output from docker image");
+        System.out.println("Thankyou ");
+    }
+}
