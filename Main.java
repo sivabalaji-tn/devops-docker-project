7ede7ba9f13e96e2fc9dd3222cdc7211e2ed0e5b
sivabalaji-tn <sivathetechie24@gmail.com>
2026-02-17 16:15:41 +0530

    Project coded remodified

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Im a Build pipeline output from docker image");
-        System.out.println("Thankyou ");
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Initiating sequence...");
+            Thread.sleep(1500); // Dramatic 1.5 second pause
+
+            System.out.println("Preparing system components...");
+            Thread.sleep(2000); // 2 second pause
+
+            System.out.println("...");
+            Thread.sleep(1000);
+
+            System.out.println("WARNING: Unconventional output detected.");
+            Thread.sleep(1500);
+
+            System.out.println("Project effect complete.");
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted!");
+        }
     }
 }
